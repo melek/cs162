aeed1f85fab2994b682bf6ce7df57bca9c4878ed
Lionel Di Giacomo <lionel.digiacomo@pcc.edu>
2020-10-08 14:57:40 -0700

    First use of Make

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -1,11 +1,26 @@
 #include "main.h"
 
+// 'class' here could be 'typename'; they are equivalent.
+template <class T>
+T add2(T a) {
+    return a + 2;
+}
+
 int main(int argc, char **args, char **envp)
 {
     std::cout << "Testing the C Standard Library" << std::endl;
 
     std::cout << "1. assert.h: void assert(int)" << std::endl;
-    assert(false);
+    assert(true);
+
+    std::cout << "2. Template: ";
+        int a = 5;
+        float f = 0.5;
+        a = add2<int>(a);
+        f = add2<float>(f);
+
+    std::cout << "Int(5) + 2: " << a << " - " << f << std::endl;
+
 
     return 0;
 }
--- sandbox/main.h
+++ sandbox/main.h
@@ -1,7 +1,10 @@
+#pragma once
+
 #include <iostream>
+#include <fstream>
+
 // Uncomment the line below to disable assert.h for production
 // #define NDEBUG 
 #include <assert.h>
-
 
 int main(int, char **, char **);
